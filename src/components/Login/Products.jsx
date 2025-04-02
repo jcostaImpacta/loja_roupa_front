6bd4921c21305edf0c8cdd2dbbe1ab3f2bf9b2b9
Débora_vitória <88170747+dboravitoria@users.noreply.github.com>
2025-04-01 22:55:58 -0300

    ajustando filtros

--- src/components/Login/Products.jsx
+++ src/components/Login/Products.jsx
@@ -6,6 +6,7 @@ import {
 } from "@mui/material";
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import PaidIcon from '@mui/icons-material/Paid';
 import TuneIcon from '@mui/icons-material/Tune';
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircle from '@mui/icons-material/AccountCircle';
@@ -13,35 +14,43 @@ import './Products.css';
 import { useNavigate } from "react-router-dom";
 
 const ProductGrid = () => {
-  const username = localStorage.getItem("username");
+  const [username, setUsername] = useState("");
   const navigate = useNavigate();
   const [produtos, setProdutos] = useState([]);
   const [loading, setLoading] = useState(true);
   const [categorias, setCategorias] = useState([]);
   const [search, setSearch] = useState("");
   const [anchorEl, setAnchorEl] = useState(null);
-  const [filters, setFilters] = useState({ categoria: "", genero: "", valorMin: 1, valorMax: 300 });
+  const [filters, setFilters] = useState({valorMin: 1, valorMax: 300 });
+  const [aplicarFiltro, setAplicarFiltro] = useState(true)
   const [page, setPage] = useState(1);
   const itemsPerPage = 10;
 
   useEffect(() => {
     setLoading(true);
-    fetch("/api/get_products/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(filters),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setProdutos(data);
-        setLoading(false);
-      })
-      .catch(() => setLoading(false));
-  }, [filters]);
-
   
+    // Fazendo as duas requisições ao mesmo tempo
+    Promise.all([
+      fetch("/api/get_products/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(filters),
+      }).then((res) => res.json()),
+  
+      fetch("/api/list/categoria/")
+        .then((res) => res.json())
+    ])
+      .then(([produtosData, categoriasData]) => {
+        setProdutos(produtosData);
+        setCategorias(categoriasData); 
+      })
+      .catch((error) => console.error("Erro nas requisições:", error))
+      .finally(() => setLoading(false));
+  }, [aplicarFiltro]);
+    
+  const handleClick = () =>{
+    setAplicarFiltro()
+  }
   const handleFilterChange = (event) => {
     setFilters({ ...filters, [event.target.name]: event.target.value });
   };
@@ -56,20 +65,12 @@ const ProductGrid = () => {
 
   const handleMenuClose = () => {
     setAnchorEl(null);
+  };
+  
+  const handleLogout = () => {
+    setAnchorEl(null);
     navigate("/");
   };
-
-  const searchValue = search || ""; 
-
-  const filteredProducts = produtos.filter(produto => {
-    return (
-      (produto.dc_produto || "").toLowerCase().includes(searchValue.toLowerCase()) &&
-      (filters.categoria ? (produto.categoria || "").toLowerCase() === filters.categoria.toLowerCase() : true) &&
-      (filters.genero ? (produto.genero || "").toLowerCase() === filters.genero.toLowerCase() : true) &&
-      produto.vl_produto >= filters.valorMin && produto.vl_produto <= filters.valorMax
-    );
-  });
-  
 
 const handleSliderChange = (event, newValue) => {
   setFilters({
@@ -79,26 +80,22 @@ const handleSliderChange = (event, newValue) => {
   });
 };
 
+  const searchValue = search || ""; 
+
+  const filteredProducts = produtos.filter(produto => {
+    return (
+      (produto.dc_produto || "").toLowerCase().includes(searchValue.toLowerCase())
+    )
+  });
+  
 const paginatedProducts = filteredProducts.slice((page - 1) * itemsPerPage, page * itemsPerPage);
 
   return (
     <Box sx={{ display: "flex", backgroundColor: "#ccc", height: "100vh", width: "100vw" }}>
-      <Drawer
-        variant="permanent"
-        sx={{
-          width: "10vw",
-          flexShrink: 0,
-          "& .MuiDrawer-paper": { width: "10", backgroundImage: "linear-gradient(45deg, #0C2051,#2EAAE9)", },
-        }}
-      >
+      <Drawer variant="permanent" sx={{width: "10vw",flexShrink: 0,"& .MuiDrawer-paper": { width: "10", backgroundImage: "linear-gradient(45deg, #0C2051,#2EAAE9)", },}}>
         <Toolbar >
           <Typography variant="h6">
-            <Box
-              component="img"
-              src="logoClothes.png"
-              alt="logo"
-              sx={{ width: 180, height: "auto", marginLeft: "none" }}
-            />
+            <Box component="img" src="logoClothes.png" alt="logo" sx={{ width: 180, height: "auto", marginLeft: "none" }}/>
           </Typography>
         </Toolbar>
         <AppBar position="static" sx={{ backgroundColor: "transparent", boxShadow: "none" }}>
@@ -112,94 +109,81 @@ const paginatedProducts = filteredProducts.slice((page - 1) * itemsPerPage, page
       </Drawer>
 
       <Box sx={{ width: "70vw", ml: "10vw", mt: 2 }}>
-        <TextField
-          placeholder="Buscar..."
-          variant="outlined"
-          fullWidth
-          value={search}
-          onChange={handleSearchChange}
-          sx={{
-            width: "25vw", marginTop: "2vh", marginBottom: '5vh',
-            backgroundColor: "#eee", borderRadius: "8px", boxShadow: 3, "&:hover": { backgroundColor: "none", border: "#001469" }
-          }}
-          InputProps={{
-            endAdornment: (
+        <TextField placeholder="Buscar..." variant="outlined" fullWidth value={search} onChange={handleSearchChange} sx={{width: "25vw", marginTop: "1vh", marginBottom: '3vh',backgroundColor: "#eee", borderRadius: "8px", boxShadow: 3, "&:hover": { backgroundColor: "none", border: "#001469" }
+          }} InputProps={{ endAdornment: (
               <InputAdornment position="end">
                 <SearchIcon sx={{ color: "#001469" }} />
-              </InputAdornment>
-            ),
-          }}
-        />
+              </InputAdornment>),}}/>
         <Button edge="start" color="inherit" onClick={handleMenuOpen} sx={{ backgroundColor: "none", boxShadow: "none", width: '1vw', marginLeft:'-2vw', "&:hover": { backgroundColor: "none" } }}>
           <AccountCircle sx={{ height: '4vh', width: '4vw', marginLeft: "80vw", marginTop: '4vh', backgroundColor: "none", color: "#001469", "&:hover": { backgroundColor: "none", boxShadow: "none" } }} />
           <Typography variant="h6" sx={{ color: "#001469", marginTop: "4vh", backgroundColor: "none", boxShadow: "none", "&:hover": { backgroundColor: "none", boxShadow: "none" } }}>
-          {username ? `${username}` : "Olá, Usuário!"}
+          {username ? username : "Olá"} 
+          {/* estou com dúvida nesse pedaço de código, era pra retornar o nome do usuário logado */}
         </Typography>
         </Button>
-        <Menu
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleMenuClose}
-          sx={{ marginLeft: "40vw", marginTop: "2vh", backgroundColor: "none" }}
-        >
-          <MenuItem onClick={handleMenuClose} sx={{ backgroundColor: "none", boxShadow: "none", color: "#001469", "&:hover": { backgroundColor: "none", boxShadow: "none" } }}>Sair </MenuItem>
+        <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose} sx={{ marginLeft: "40vw", marginTop: "2vh", backgroundColor: "none" }}>
+          <MenuItem onClick={handleLogout} sx={{ backgroundColor: "none", boxShadow: "none", color: "#001469", "&:hover": { backgroundColor: "none", boxShadow: "none" } }}>Sair </MenuItem>
         </Menu>
-
-        <Paper sx={{ width: "70vw", p: 2, display: 'flex', marginBottom: "1vh", boxShadow: 3, backgroundColor: "#eee", borderRadius: "8px" }}>
-          <TuneIcon sx={{ color: "#001469", marginRight: "1vw" }}>Filtros</TuneIcon>
+        <Paper sx={{ width: "70vw", p: 2, mb: "2vh", boxShadow: 3, backgroundColor: "#eee",borderRadius: "8px",}}>
+        <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", mb: 2, width: "55%"}}>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <TuneIcon sx={{ color: "#001469", mr: 1 }} />
           <Typography sx={{ color: "#001469", fontWeight: "bold" }}>Filtros</Typography>
-          <Box sx={{ display: "flex", width: "40vw", backgroundColor: "#ccc", borderRadius: "8px", padding: 2, boxShadow: 3, height: '9vh', marginBottom: "1vh", marginTop: "4vh", marginLeft: "1vw", marginRight: "2vw" }}>
-            <FormControl fullWidth>
-              <InputLabel>Categoria</InputLabel>
-              <Select
-                name="categoria"
-                value={filters.categoria}
-                onChange={handleFilterChange}
-                sx={{ width: "15vw", marginRight: "1vw" }}
-                label="Categoria"
-              >
-                <MenuItem value="">Todas</MenuItem>
-                <MenuItem value="Biquini">Biquini</MenuItem>
-                <MenuItem value="Body">Body</MenuItem>
-                <MenuItem value="Calça">Calça</MenuItem>
-                <MenuItem value="Camiseta">Camiseta</MenuItem>
-                <MenuItem value="Camisa">Camisa</MenuItem>
-                <MenuItem value="Óculos">Óculos</MenuItem>
-                <MenuItem value="Sandália">Sandália</MenuItem>
-                <MenuItem value="Shorts">Shorts</MenuItem>
-                <MenuItem value="Sunga">Sunga</MenuItem>
-                <MenuItem value="Tênis">Tênis</MenuItem>
-                <MenuItem value="Vestido">Vestido</MenuItem>
-                {categorias.length > 0 && categorias.map((categoria) => (
-                <MenuItem key={categoria.id} value={categoria.nome}>
-                  {categoria.nome}
-                </MenuItem>
-              ))}
+        </Box>
+        <Box sx={{ display: "flex", alignItems: "", mr: "-35%" }}>
+          <PaidIcon sx={{ color: "#001469", mr: 1 }} />
+          <Typography sx={{ color: "#001469", fontSize: 16, fontWeight: "bold" }}>Preço</Typography>
+        </Box>
+        </Box>
+         <Box sx={{display: "flex",alignItems: "center",gap: 2,flexWrap: "wrap",mb: 2,}}>
 
+            <FormControl sx={{ minWidth: "10vw" }}>
+              <InputLabel>Categoria</InputLabel>
+              <Select name="categoria" value={filters.categoria} onChange={handleFilterChange}label="Categoria">
+              <MenuItem>Todas</MenuItem>
+                {categorias.map((categoria) => (
+                <MenuItem key={categoria.dc_categoria} value={categoria.id_categoria}> {categoria.dc_categoria}</MenuItem>
+                ))}
               </Select>
             </FormControl>
-            <FormControl fullWidth>
+            <FormControl sx={{ minWidth: "10vw" }}>
               <InputLabel>Gênero</InputLabel>
-              <Select
-                name="genero"
-                value={filters.genero}
-                onChange={handleFilterChange}
-                sx={{ width: "15vw" }}
-                label="Gênero"
-              >
+              <Select name="genero" value={filters.genero}onChange={handleFilterChange}label="Gênero">
                 <MenuItem value="">Todos</MenuItem>
                 <MenuItem value="Masculino">Masculino</MenuItem>
                 <MenuItem value="Feminino">Feminino</MenuItem>
                 <MenuItem value="Unissex">Unissex</MenuItem>
               </Select>
             </FormControl>
-          </Box>
-          <Box sx={{ display: "flex", flexDirection: "column", width: "40vw", backgroundColor: "#ccc", borderRadius: "8px", padding: "1vw", marginBottom: "1vh", boxShadow: 3, marginRight: "2vw", height: '9vh', marginTop: "4vh", marginLeft: "1vw" }}>
-            <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-              <Typography sx={{ color: '#001469' }}>Mínimo: R${filters.valorMin}</Typography>
-              <Typography sx={{ color: '#001469' }}>Máximo: R${filters.valorMax}</Typography>
+            <FormControl sx={{}}>
+              <FormControl sx={{ minWidth: "10vw" }}>
+                <InputLabel>Público</InputLabel>
+                <Select name="publico" value={filters.publico} onChange={handleFilterChange}label="Público">
+                  <MenuItem value="">Todos</MenuItem>
+                  <MenuItem value="Adulto">Adulto</MenuItem>
+                  <MenuItem value="Infantil">Infantil</MenuItem>
+                </Select>
+              </FormControl>
+            </FormControl>
+            <FormControl sx={{}}>
+              <FormControl sx={{ minWidth: "10vw" }}>
+                <InputLabel>Coleção</InputLabel>
+                <Select name="colecao" value={filters.colecao} onChange={handleFilterChange}label="Coleção">
+                  <MenuItem value="">Todas</MenuItem>
+                  <MenuItem value="Verão">Verão</MenuItem>
+                  <MenuItem value="Inverno">Inverno</MenuItem>
+                </Select>
+              </FormControl>
+            </FormControl>
+            
+            <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1}}>
+              <Typography sx={{ color: "#001469", mr: 1, fontSize:16}}> R${filters.valorMin}</Typography>
+              <Slider value={[filters.valorMin, filters.valorMax]} onChange={handleSliderChange} valueLabelDisplay="auto" valueLabelFormat={(value) => `R$ ${value}`} min={1} max={300} sx={{ color: "#001469", flexGrow: 1, mx: 2 }}/>
+              <Typography sx={{ color: "#001469", ml: 1 }}>R${filters.valorMax}</Typography>
             </Box>
-            <Slider value={[filters.valorMin, filters.valorMax]} onChange={handleSliderChange} valueLabelDisplay="auto" valueLabelFormat={(value) => `R$ ${value}`} min={1} max={300} sx={{ marginBottom: "1vh", color: '#001469' }} />
+          </Box>
+          <Box sx={{ display: "flex", width: "16%", justifyContent: "flex-end", mx: "83%" }}>  
+            <Button variant="contained" onChange={handleClick} sx={{ backgroundColor: "#001469", color: "#ccc",textTransform:"capitalize", fontWeight:"bold",":hover": {backgroundColor: "#003399"}}}>Aplicar Filtros</Button>
           </Box>
         </Paper>
         <TableContainer component={Paper}>
@@ -215,9 +199,7 @@ const paginatedProducts = filteredProducts.slice((page - 1) * itemsPerPage, page
             <TableBody>
               {loading ? (
                 <TableRow>
-                  <TableCell colSpan={4} align="center">
-                    <CircularProgress />
-                  </TableCell>
+                  <TableCell colSpan={4} align="center"><CircularProgress /></TableCell>
                 </TableRow>
               ) : (
                 paginatedProducts.map((produto) => (
@@ -226,17 +208,13 @@ const paginatedProducts = filteredProducts.slice((page - 1) * itemsPerPage, page
                     <TableCell>{produto.dc_produto}</TableCell>
                     <TableCell>{produto.qtd_produto}</TableCell>
                     <TableCell>{produto.vl_produto}</TableCell>
-                  </TableRow>
-                ))
+                  </TableRow>))
               )}
             </TableBody>
           </Table>
         </TableContainer>
         <Box sx={{ display: "flex", justifyContent: "flex-end", mt:1}}>
-          <Pagination count={Math.ceil(filteredProducts.length / itemsPerPage)} page={page} onChange={(e, value) => setPage(value)} sx={{"& .MuiPaginationItem-root": {color: "#001469"},"& .MuiPaginationItem-previousNext": {backgroundColor: "#001469",color:"#ddd","&:hover": {backgroundColor: "#003399"}
-              },
-            }}
-          />
+          <Pagination count={Math.ceil(filteredProducts.length / itemsPerPage)} page={page} onChange={(e, value) => setPage(value)} sx={{"& .MuiPaginationItem-root": {color: "#001469"},"& .MuiPaginationItem-previousNext": {backgroundColor: "#001469",color:"#ddd","&:hover": {backgroundColor: "#003399"}},}}/>
         </Box>
       </Box>
     </Box>
